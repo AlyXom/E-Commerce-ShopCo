1a22b4cb15f12a52413a527d298e39799aaabb25
Alysson <alyssoncraft12@gmail.com>
2024-08-10 05:58:09 -0300

    feat/modal toggle and drawer toggle

--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
-const hamburger = document.getElementById("hamburger")
+const hamburger = document.getElementById("hamburger") as HTMLImageElement
 const modal = document.getElementById("modal") as HTMLDivElement
+const drawer = document.getElementsByClassName("drawer") as HTMLCollectionOf<HTMLDivElement>
 
 function modalTrueOrFalse() {
     if(modal.style.left == "0px") {
-        modal.style.left = "-1000px"
+        modal.style.left = "-1400px"
     } else {
         modal.style.left = "0px"
     }
 }
+
+function drawerTrueOrFalse() {
+
+    const deskDrawer = drawer[0]
+    const mobileDrawer = drawer[1]
+
+    if(deskDrawer.style.height == "0px" && mobileDrawer.style.height == "0px") {
+        deskDrawer.style.height = "70px"
+        mobileDrawer.style.height = "70px"
+    } else {
+        deskDrawer.style.height = "0px"
+        mobileDrawer.style.height = "0px"
+    }
+}
+
+console.log(drawer)
--- src/dist/index.js
+++ src/dist/index.js
@@ -1,11 +1,25 @@
 "use strict";
 const hamburger = document.getElementById("hamburger");
 const modal = document.getElementById("modal");
+const drawer = document.getElementsByClassName("drawer");
 function modalTrueOrFalse() {
     if (modal.style.left == "0px") {
-        modal.style.left = "-1000px";
+        modal.style.left = "-1400px";
     }
     else {
         modal.style.left = "0px";
     }
 }
+function drawerTrueOrFalse() {
+    const deskDrawer = drawer[0];
+    const mobileDrawer = drawer[1];
+    if (deskDrawer.style.height == "0px" && mobileDrawer.style.height == "0px") {
+        deskDrawer.style.height = "70px";
+        mobileDrawer.style.height = "70px";
+    }
+    else {
+        deskDrawer.style.height = "0px";
+        mobileDrawer.style.height = "0px";
+    }
+}
+console.log(drawer);
