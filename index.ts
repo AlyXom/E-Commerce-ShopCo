e62782ddc93aed300ccc5c907dbcfe00835a473d
Alysson <alyssoncraft12@gmail.com>
2024-08-12 23:49:13 -0300

    feat/new arrivals

--- index.ts
+++ index.ts
@@ -3,24 +3,7 @@ const modal = document.getElementById("modal") as HTMLDivElement
 const drawer = document.getElementsByClassName("drawer") as HTMLCollectionOf<HTMLDivElement>
 const searchBarMobile = document.getElementById("searchBarMobile") as HTMLDivElement
 const popup = document.getElementById("popup") as HTMLElement
-
-interface Clothes {
-    name: string,
-    image: string,
-    stars: number,
-    price: number,
-    discount: number
-}
-
-const new_arrival: Clothes[] = [
-    {
-        name: "T-shirt with Tape Details",
-        image: "src/assets/clothes/t-shirt-1.png",
-        price: 120,
-        discount: 0,
-        stars: 4.5
-    }
-] 
+const clothes = document.getElementById("clothes") as HTMLElement
 
 function modalTrueOrFalse() {
     if(modal.style.left == "0px") {
@@ -55,3 +38,82 @@ function showOrHideBar() {
 function closePopup() {
     popup.style.height = "0px"
 }
+
+function reviewRate(stars: number) {
+    let starsHtml = ""
+    for(var n = 0; n <= stars; n++) {
+        var midstar = stars - n
+
+        
+
+        if(midstar == 0.5) {
+            starsHtml += "<img src='src/assets/icons/mid-star.svg' />"
+        } else {
+            starsHtml += "<img src='src/assets/icons/rating-star.svg' />"
+        }
+    }
+
+    return starsHtml
+}
+
+interface Clothes {
+    name: string,
+    image: string,
+    stars: number,
+    price: number,
+    discount: number
+}
+
+const new_arrival: Clothes[] = [
+    {
+        name: "T-shirt with Tape Details",
+        image: "src/assets/clothes/t-shirt-1.png",
+        price: 120,
+        discount: 0,
+        stars: 4.5
+    },
+    {
+        name: "Skinny Fit Jeans",
+        image: "src/assets/clothes/pants-1.png",
+        price: 240,
+        discount: 20,
+        stars: 3.5
+    },
+    {
+        name: "Checkered Shirt",
+        image: "src/assets/clothes/t-shirt-2.png",
+        price: 180,
+        discount: 0,
+        stars: 4.5
+    },
+    {
+        name: "Sleeve Striped T-shirt",
+        image: "src/assets/clothes/t-shirt-3.png",
+        price: 160,
+        discount: 30,
+        stars: 4.5
+    }
+]
+
+
+new_arrival.forEach(({name, image, price, discount, stars}) => {
+    clothes.innerHTML += `
+        <div class="cards">
+            <section>
+                <img class="card-image" src="${image}"/>
+            </section>
+            <article>
+                <p>${name}</p>
+                <span>
+                ${reviewRate(stars)}
+                <p>${stars}/<span>5</span></p>
+                </span>
+                <div id="infos">
+                    <p>$${price - ((discount / 100) * price)}</p>
+                    <p class="discount">${discount == 0 ? "" : `$${price}`}</p>
+                    ${discount == 0 ? `<span></span>` : `<span class="float-discount">-${discount}%</span>`}
+                </div>
+            </article>
+        </div>
+    `
+})
--- src/dist/index.js
+++ src/dist/index.js
@@ -4,6 +4,7 @@ const modal = document.getElementById("modal");
 const drawer = document.getElementsByClassName("drawer");
 const searchBarMobile = document.getElementById("searchBarMobile");
 const popup = document.getElementById("popup");
+const clothes = document.getElementById("clothes");
 function modalTrueOrFalse() {
     if (modal.style.left == "0px") {
         modal.style.left = "-1400px";
@@ -35,3 +36,67 @@ function showOrHideBar() {
 function closePopup() {
     popup.style.height = "0px";
 }
+function reviewRate(stars) {
+    let starsHtml = "";
+    for (var n = 0; n <= stars; n++) {
+        var midstar = stars - n;
+        if (midstar == 0.5) {
+            starsHtml += "<img src='src/assets/icons/mid-star.svg' />";
+        }
+        else {
+            starsHtml += "<img src='src/assets/icons/rating-star.svg' />";
+        }
+    }
+    return starsHtml;
+}
+const new_arrival = [
+    {
+        name: "T-shirt with Tape Details",
+        image: "src/assets/clothes/t-shirt-1.png",
+        price: 120,
+        discount: 0,
+        stars: 4.5
+    },
+    {
+        name: "Skinny Fit Jeans",
+        image: "src/assets/clothes/pants-1.png",
+        price: 240,
+        discount: 20,
+        stars: 3.5
+    },
+    {
+        name: "Checkered Shirt",
+        image: "src/assets/clothes/t-shirt-2.png",
+        price: 180,
+        discount: 0,
+        stars: 4.5
+    },
+    {
+        name: "Sleeve Striped T-shirt",
+        image: "src/assets/clothes/t-shirt-3.png",
+        price: 160,
+        discount: 30,
+        stars: 4.5
+    }
+];
+new_arrival.forEach(({ name, image, price, discount, stars }) => {
+    clothes.innerHTML += `
+        <div class="cards">
+            <section>
+                <img class="card-image" src="${image}"/>
+            </section>
+            <article>
+                <p>${name}</p>
+                <span>
+                ${reviewRate(stars)}
+                <p>${stars}/<span>5</span></p>
+                </span>
+                <div id="infos">
+                    <p>$${price - ((discount / 100) * price)}</p>
+                    <p class="discount">${discount == 0 ? "" : `$${price}`}</p>
+                    ${discount == 0 ? `<span></span>` : `<span class="float-discount">-${discount}%</span>`}
+                </div>
+            </article>
+        </div>
+    `;
+});
